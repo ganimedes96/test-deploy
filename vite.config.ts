a6e53551186a77df56cbf15ba9dc84bb40534222
Juno <ganimedes409@gmail.com>
2023-10-31 18:26:41 -0300

    chore

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8080, // default: 3000
+    host: '0.0.0.0' // default: localhost
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
