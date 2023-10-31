c2fb07b1739b9c94f76dae43f4b66177c756bfed
Juno <ganimedes409@gmail.com>
2023-10-31 17:38:50 -0300

    chore

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: 'https://hammerhead-app-mvicn.ondigitalocean.app'
 })
 
 export const socket = axios.create({
