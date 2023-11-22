cb43a50cefac229bdd6fb329440cec4977936fa3
Juno <ganimedes409@gmail.com>
2023-11-22 12:19:06 -0300

    chore

--- src/pages/pix/index.tsx
+++ src/pages/pix/index.tsx
@@ -90,7 +90,7 @@ export default function Pix() {
     });
     // Remova o ouvinte quando o componente for desmontado para evitar vazamento de memória
     return () => {
-      socket.off('confirmPayment');
+      socket.off('payment');
     };
   }, []);
 
