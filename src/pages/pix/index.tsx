29bfbc3bc7e9e4308662d66793198a1349839d8f
Juno <ganimedes409@gmail.com>
2023-11-24 11:40:25 -0300

    chore

--- src/pages/pix/index.tsx
+++ src/pages/pix/index.tsx
@@ -94,10 +94,20 @@ export default function Pix() {
     };
   }, []);
 
-  socket.emit('PaymentSuccessRoom', {
-    roomId: id
-  })
-
+  useEffect(() => {
+    socket.on('PaymentSuccessRoom', (data) => {
+      console.log(data, 'PaymentSuccessRoom-Front');
+      
+      socket.emit('PaymentSuccessRoom', {
+        roomId: id
+      })
+      socket.emit('payment', {
+       status: data.status,
+       roomId: id 
+      }) 
+    })
+    
+  }, [])
   
 
   const getDataCookies = () => {
